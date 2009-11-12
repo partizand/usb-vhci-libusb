88a79e640e76f9a49a247a2bb3834c70e8ea6a40
Michael Singer <michael@a-singer.de>
2009-11-12 23:24:16 +0100

    usb_vhci_fetch_work implemented
    + fixes and cleanups

--- src/libusb_vhci.c
+++ src/libusb_vhci.c
@@ -26,22 +26,25 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 #include "vhci-hcd.h"
 #include "libusb_vhci.h"
 
-int usb_vhci_open(uint8_t port_count, int32_t *id, int32_t *usb_busnum, char *bus_id)
+int usb_vhci_open(uint8_t port_count,  // [IN]  number of ports
+                  int32_t *id,         // [OUT] controller id
+                  int32_t *usb_busnum, // [OUT] usb bus number
+                  char    *bus_id)     // [OUT] bus id (usually
+                                       //       vhci_hcd.<controller id>)
 {
-	int fd, err;
-	struct vhci_ioc_register r;
-
-	fd = open("/dev/vhci-ctrl", O_RDWR);
+	int fd = open(USB_VHCI_DEVICE_FILE, O_RDWR);
 	if(fd == -1) return -1;
 
+	struct vhci_ioc_register r;
 	r.port_count = port_count;
 	if(ioctl(fd, VHCI_HCD_IOCREGISTER, &r) == -1)
 	{
-		err = errno;
+		int err = errno;
 		usb_vhci_close(fd);
 		errno = err;
 		return -1;
@@ -61,13 +64,14 @@ int usb_vhci_open(uint8_t port_count, int32_t *id, int32_t *usb_busnum, char *bu
 
 int usb_vhci_close(int fd)
 {
-	return TEMP_FAILURE_RETRY(close(fd));
+	int result;
+	while((result = close(fd)) == -1 && errno == EINTR);
+	return result;
 }
 
 int usb_vhci_fetch_work(int fd, struct usb_vhci_work *work)
 {
 	struct vhci_ioc_work w;
-
 	if(ioctl(fd, VHCI_HCD_IOCFETCHWORK, &w) == -1)
 		return -1;
 
@@ -77,13 +81,64 @@ int usb_vhci_fetch_work(int fd, struct usb_vhci_work *work)
 		work->type = USB_VHCI_WORK_TYPE_PORT_STAT;
 		work->work.portstat.status = w.work.port.status;
 		work->work.portstat.change = w.work.port.change;
-		work->work.portstat.index = w.work.port.index;
-		work->work.portstat.flags = w.work.port.flags;
+		work->work.portstat.index  = w.work.port.index;
+		work->work.portstat.flags  = w.work.port.flags;
 		return 0;
+
 	case VHCI_IOC_WORK_TYPE_PROCESS_URB:
+		memset(&work->work.urb, 0, sizeof work->work.urb);
+		switch(w.work.urb.type)
+		{
+		case VHCI_IOC_URB_TYPE_ISO:
+			work->work.urb.type          = USB_VHCI_URB_TYPE_ISO;
+			work->work.urb.packet_count  = w.work.urb.packet_count;
+			work->work.urb.interval      = w.work.urb.interval;
+			break;
+		case VHCI_IOC_URB_TYPE_INT:
+			work->work.urb.type          = USB_VHCI_URB_TYPE_INT;
+			work->work.urb.interval      = w.work.urb.interval;
+			break;
+		case VHCI_IOC_URB_TYPE_CONTROL:
+			work->work.urb.type          = USB_VHCI_URB_TYPE_CONTROL;
+			work->work.urb.wValue        = w.work.urb.setup_packet.wValue;
+			work->work.urb.wIndex        = w.work.urb.setup_packet.wIndex;
+			work->work.urb.wLength       = w.work.urb.setup_packet.wLength;
+			work->work.urb.bmRequestType = w.work.urb.setup_packet.bmRequestType;
+			work->work.urb.bRequest      = w.work.urb.setup_packet.bRequest;
+			break;
+		case VHCI_IOC_URB_TYPE_BULK:
+			work->work.urb.type          = USB_VHCI_URB_TYPE_BULK;
+			work->work.urb.flags         = ((w.work.urb.flags & VHCI_IOC_URB_FLAGS_SHORT_NOT_OK) ?
+			                                USB_VHCI_URB_FLAGS_SHORT_NOT_OK : 0) |
+			                               ((w.work.urb.flags & VHCI_IOC_URB_FLAGS_ZERO_PACKET) ?
+			                                USB_VHCI_URB_FLAGS_ZERO_PACKET : 0);
+			break;
+		default:
+			errno = EBADMSG;
+			return -1;
+		}
+		work->type = USB_VHCI_WORK_TYPE_PROCESS_URB;
+		work->work.urb.status        = -EINPROGRESS;
+		work->work.urb.handle        = w.handle;
+		work->work.urb.buffer_length = w.work.urb.buffer_length;
+		if(usb_vhci_is_out(w.work.urb.endpoint) || usb_vhci_is_iso(work->work.urb.type))
+			work->work.urb.buffer_actual = w.work.urb.buffer_length;
+		work->work.urb.devadr        = w.work.urb.address;
+		work->work.urb.epadr         = w.work.urb.endpoint;
+		// return 1 if usb_vhci_fetch_data should be called
+		return work->work.urb.buffer_actual || work->work.urb.packet_count;
+
 	case VHCI_IOC_WORK_TYPE_CANCEL_URB:
+		work->type = USB_VHCI_WORK_TYPE_CANCEL_URB;
+		work->work.handle = w.handle;
+		return 0;
+
 	default:
-		errno = EIO;
+		errno = EBADMSG;
 		return -1;
+	}
+
+	int usb_vhci_fetch_data(int fd, const struct usb_vhci_urb *urb)
+	{
 	}
 }
--- src/libusb_vhci.h
+++ src/libusb_vhci.h
@@ -21,15 +21,39 @@
 
 #include <stdint.h>
 
+#define USB_VHCI_DEVICE_FILE "/dev/vhci-ctrl"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct usb_vhci_iso_packet
+{
+	uint32_t offset;
+	uint32_t packet_length, packet_actual;
+	int32_t status;
+};
+
 struct usb_vhci_urb
 {
 	uint64_t handle;
 	uint8_t *buffer;
+	struct usb_vhci_iso_packet *iso_packets;
 	int32_t buffer_length, buffer_actual;
+	int32_t packet_count, error_count;
+	int32_t status, interval;
 	uint16_t flags;
+#define USB_VHCI_URB_FLAGS_SHORT_NOT_OK 0x0001
+#define USB_VHCI_URB_FLAGS_ISO_ASAP     0x0002
+#define USB_VHCI_URB_FLAGS_ZERO_PACKET  0x0040
 	uint16_t wValue, wIndex, wLength;
 	uint8_t bmRequestType, bRequest;
 	uint8_t devadr, epadr;
+	uint8_t type;
+#define USB_VHCI_URB_TYPE_ISO     0
+#define USB_VHCI_URB_TYPE_INT     1
+#define USB_VHCI_URB_TYPE_CONTROL 2
+#define USB_VHCI_URB_TYPE_BULK    3
 };
 
 struct usb_vhci_portstat
@@ -53,10 +77,24 @@ struct usb_vhci_work
 #define USB_VHCI_WORK_TYPE_CANCEL_URB  2
 };
 
-int usb_vhci_open(uint8_t port_count, int32_t *id, int32_t *usb_busnum, char *bus_id);
+int usb_vhci_open(uint8_t port_count,
+                  int32_t *id,
+                  int32_t *usb_busnum,
+                  char    *bus_id);
 int usb_vhci_close(int fd);
 int usb_vhci_fetch_work(int fd, struct usb_vhci_work *work);
-int usb_vhci_fetch_data(int fd, const struct usb_vhci_work *work);
+int usb_vhci_fetch_data(int fd, const struct usb_vhci_urb *urb);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#define usb_vhci_is_out(epadr)    !((epadr) & 0x80)
+#define usb_vhci_is_in(epadr)     !!((epadr) & 0x80)
+#define usb_vhci_is_iso(type)     ((type) == USB_VHCI_URB_TYPE_ISO)
+#define usb_vhci_is_int(type)     ((type) == USB_VHCI_URB_TYPE_INT)
+#define usb_vhci_is_control(type) ((type) == USB_VHCI_URB_TYPE_CONTROL)
+#define usb_vhci_is_bulk(type)    ((type) == USB_VHCI_URB_TYPE_BULK)
 
 #endif // __LIBUSB_VHCI_H__
 
